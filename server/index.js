8605e95630b327f0a56331422b78e67767ffa9fd
Manu Y <manu9494230@gmail.com>
2024-03-23 15:53:49 +0100

    0.0.5 📋

--- server/index.js
+++ server/index.js
@@ -11,8 +11,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/ymovie', {
 }).then(() => {
   console.log('Connexion à MongoDB réussie !');
 
-  //Fonction filmData
-  injectFilmsData();
+  //filmData.js
+const Film = require('./models/filmsModel');
+const xlsx = require('xlsx');
+
+// Exporte la fonction injectFilmsData
+async function injectFilmsData() {
+  try {
+    const excelFilePath = '../database/film.xlsx';
+    const workbook = xlsx.readFile(excelFilePath);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const filmsData = xlsx.utils.sheet_to_json(worksheet);
+
+    for (const filmData of filmsData) {
+      const film = new Film(filmData);
+      await film.save();
+    }
+    console.log('Données de films injectées avec succès !');
+  } catch (error) {
+    console.error('Erreur lors de l\'injection des données de films :', error);
+  }
+}
+
+injectFilmsData()
 
   app.listen(PORT, () => {
     console.log(`Le serveur fonctionne sur http://localhost:${PORT}`);
